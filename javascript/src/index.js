88137495b6c586a7eb94771f8affc0b62d26f8f4
Matt Bengston <mattb@unity3d.com>
2017-07-28 23:42:08 +0300

    Added performance logging to main program

--- javascript/src/index.js
+++ javascript/src/index.js
@@ -1,18 +1,31 @@
-// @flow
+/**
+ * @flow
+ */
 
 'use strict';
 
 import Board from './classes/board';
 import Solver from './classes/solver';
-import parseBoard from './utils/parse-board';
+import ParseBoard from './utils/parse-board';
 import fs from 'fs';
-
 // Parse File
 const file: string = fs.readFileSync(process.argv[2]).toString();
-const tiles: number[][] = parseBoard(file);
 
+// @todo: Create more elegant performance logging wrapper for this nonsense
+let startTime;
+let parseTime;
+let boardCreateTime;
+let solverCreateTime;
+let endTime;
+
+console.log('Read file. Solving...');
+startTime = Date.now();
+const tiles: number[][] = ParseBoard(file);
+parseTime = Date.now();
 const initial = new Board(tiles);
+boardCreateTime = Date.now();
 const solver = new Solver(initial);
+solverCreateTime = Date.now();
 
 // If the puzzle is solvable, continue solving
 if (!solver.isSolvable()) {
@@ -22,9 +35,15 @@ if (!solver.isSolvable()) {
 }
 
 solver.onSolutionReady((states, moves) => {
+  endTime = Date.now();
   states.filter((state) => {
     console.log(state.board.viewBoard());
   });
   console.log(`Minimum number of moves: ${moves}`);
+  console.log(`Solution found in ${(endTime - startTime) * 1000}ms.`);
+  console.log(`\tBoard parsing:\t\t${(parseTime - startTime) * 1000}ms`);
+  console.log(`\tBoard creation:\t\t${(boardCreateTime - parseTime) * 1000}ms`);
+  console.log(`\tSolver creation:\t\t${(solverCreateTime - boardCreateTime) * 1000}ms`);
+  console.log(`\tSolving:\t\t\t${(endTime - solverCreateTime) * 1000}ms`);
   process.exit(0);
 });
